e94db4f5f960a5da58d633461afd9d1b10f56732
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-04-06 22:02:39 +0300

    Created DB models and created tables from config

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("./players.json", "rb") as players_data_json:
+        players_data = json.load(players_data_json)
+
+        for player_name, player_data in players_data.items():
+
+            player_race = Race.objects.get_or_create(
+                name=player_data["race"]["name"],
+                description=player_data["race"]["description"],
+            )
+
+            for skill_data in player_data["race"]["skills"]:
+                Skill.objects.get_or_create(
+                    race=player_race[0],
+                    **skill_data
+                )
+
+            player_guild = [None]
+
+            if player_data["guild"]:
+                player_guild = Guild.objects.get_or_create(
+                    **player_data["guild"]
+                )
+
+            Player.objects.get_or_create(
+                nickname=player_name,
+                email=player_data["email"],
+                bio=player_data["bio"],
+                race=player_race[0],
+                guild=player_guild[0]
+            )
 
 
 if __name__ == "__main__":
