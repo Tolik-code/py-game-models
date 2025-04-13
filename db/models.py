3b506709c4c9ff09d23713171d691915b3655ced
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-04-13 15:59:17 +0300

    Switched implementation to dict get

--- db/models.py
+++ db/models.py
@@ -9,7 +9,7 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.SET_NULL)
+    race = models.ForeignKey(Race, on_delete=models.SET_NULL, null=True)
 
 
 class Guild(models.Model):
@@ -21,7 +21,7 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=False, max_length=255)
     bio = models.CharField(max_length=255, blank=True, null=True)
-    race = models.ForeignKey(Race, on_delete=models.SET_NULL)
+    race = models.ForeignKey(Race, on_delete=models.SET_NULL, null=True)
     guild = models.ForeignKey(
         Guild, on_delete=models.SET_NULL, null=True
     )
--- main.py
+++ main.py
@@ -10,31 +10,29 @@ def main() -> None:
         players_data = json.load(players_data_json)
 
         for player_name, player_data in players_data.items():
-
+            print(player_data, "player_data")
+            player_data_race = player_data.get("race", dict())
             player_race = Race.objects.get_or_create(
-                name=player_data["race"]["name"],
-                description=player_data["race"]["description"],
+                name=player_data_race.get("name"),
+                description=player_data_race.get("description"),
             )
 
-            for skill_data in player_data["race"]["skills"]:
+            for skill_data in player_data_race.get("skills"):
                 Skill.objects.get_or_create(
                     race=player_race[0],
                     **skill_data
                 )
 
-            player_guild = [None]
-
-            if player_data["guild"]:
-                player_guild = Guild.objects.get_or_create(
-                    **player_data["guild"]
-                )
-
             Player.objects.get_or_create(
                 nickname=player_name,
-                email=player_data["email"],
-                bio=player_data["bio"],
+                email=player_data.get("email"),
+                bio=player_data.get("bio"),
                 race=player_race[0],
-                guild=player_guild[0]
+                guild=(
+                    Guild.objects.get_or_create(
+                        **player_data.get("guild")
+                    )[0] if player_data.get("guild") else None
+                )
             )
 
 
